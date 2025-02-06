6529c427302e2a2fe055552efffde6efa45c9117
Juan Camilo <camilohoyos2251@gmail.com>
2025-02-06 18:23:22 -0500

    crear el modelo y la miragion

--- app/Models/Alumno.php
+++ app/Models/Alumno.php
@@ -8,4 +8,18 @@ use Illuminate\Database\Eloquent\Model;
 class Alumno extends Model
 {
     use HasFactory;
+
+    protected $table = 'alumnos';
+
+    protected $fillable = [
+        'nombre',
+        'fecha_nacimiento',
+        'nombre_padre',
+        'nombre_madre',
+        'grado',
+        'seccion',
+        'fecha_ingreso',
+    ];
+
+    public $timestamps = true;
 }

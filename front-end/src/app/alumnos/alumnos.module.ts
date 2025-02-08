c714840d8ea888cb020b3c9fff55b7657f5bab29
Juan Camilo <camilohoyos2251@gmail.com>
2025-02-08 00:14:07 -0500

    Base de angular carpetas y rutas

---
+++ front-end/src/app/alumnos/alumnos.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { AlumnosRoutingModule } from './alumnos-routing.module';
+import { LayoutPageComponent } from './pages/layout-page/layout-page.component';
+import { NewPageComponent } from './pages/new-page/new-page.component';
+import { ListPageComponent } from './pages/list-page/list-page.component';
+import { AlumnoPageComponent } from './pages/alumno-page/alumno-page.component';
+import { SearchPageComponent } from './pages/search-page/search-page.component';
+
+
+@NgModule({
+  declarations: [
+    LayoutPageComponent,
+    NewPageComponent,
+    ListPageComponent,
+    AlumnoPageComponent,
+    SearchPageComponent
+  ],
+  imports: [
+    CommonModule,
+    AlumnosRoutingModule
+  ]
+})
+export class AlumnosModule { }
